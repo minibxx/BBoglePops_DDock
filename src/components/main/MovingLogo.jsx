48f0e1153702c1327f1326bae4df774fe77848d1
minibxx <alsdbqls1004@gmail.com>
2024-05-09 16:19:43 +0900

    style: 흐르는 로고애니메이션 구현

--- src/components/main/MovingLogo.jsx
+++ src/components/main/MovingLogo.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { styled } from 'styled-components';
+import DDock from '/images/ddockEng.png';
 
+const Move = styled.button`
+animation: moveLeft 20s linear infinite;
+@keyframes moveLeft {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+}
+`;
 function MovingLogo() {
   return (
     <>
-        <div className='mt-[97px]'></div>
-        <div className='h-[120px] bg-[white]'></div>
+      <div className='mt-[150px]'></div>
+      <Move className='h-[130px] flex gap-[40px] pt-[20px]'>
+        <img src={DDock} className='w-[60%]'/>
+        <img src={DDock} className='w-[60%]'/>
+        <img src={DDock} className='w-[60%]'/>
+        <img src={DDock} className='w-[60%]'/>
+      </Move>
     </>
   )
 }
