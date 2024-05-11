29472911d9b3671bbaaf00a5bb1b81f15f4fecc3
minibxx <alsdbqls1004@gmail.com>
2024-05-12 03:26:04 +0900

    test: TTS 테스트

--- src/components/interview/RandomQ.jsx
+++ src/components/interview/RandomQ.jsx
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Speech from "speak-tts";
 
 function RandomQ({myJobQuestion}) {
+  useEffect(() => {
+    var voices = window.speechSynthesis.getVoices();
+    // console.log(voices.filter(item => item.lang == 'ko-KR'))
+    console.log(voices.filter(item => item.lang == 'ko-KR'))
+  }, [])
+  const speech = new Speech();
+  speech
+    .init({
+      lang: "ko-KR",
+      voice:'Google 한국의',
+      pitch: 0
+    })
+    .then((data) => {
+      // The "data" object contains the list of available voices and the voice synthesis params
+      console.log("Speech is ready, voices are available", data);
+    })
+    .catch((e) => {
+      console.error("An error occured while initializing : ", e);
+    });
+
+  const handleClick = () => {
+    speech
+      .speak({
+        text: myJobQuestion[0]
+      })
+      .then(() => {
+        console.log("Success !");
+      })
+      .catch((e) => {
+        console.error("An error occurred :", e);
+      });
+  };
+
   return (
     <>
       <div className='flex flex-row gap-[15px] mt-[15px] ml-[10px] h-[30px]'>
@@ -11,6 +45,7 @@ function RandomQ({myJobQuestion}) {
           {myJobQuestion.map((item)=>{
             return <div>{item}</div>
           })}
+           <button onClick={handleClick}>Speak</button>
         </div>
       </div>
     </>
