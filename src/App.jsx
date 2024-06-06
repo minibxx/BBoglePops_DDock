aa65491eff108cc5978bd3980eaafb90bdebee27
minibxx <alsdbqls1004@gmail.com>
2024-06-06 17:09:58 +0900

    fix: 빌드 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import './App.css'
 import React, { useRef, useState } from 'react';
-import axios from 'axios';
 import Main from '@components/main/Main.jsx'
-import Users from './components/users/Login';
 
 function App() {
-  const worker = new Worker(new URL('./worker.js', import.meta.url))
   return (
     <>
       <Main/>
