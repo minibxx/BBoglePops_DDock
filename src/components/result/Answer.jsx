37c5fa6460c90abc2621e673ea76dd85e9db9d56
minibxx <alsdbqls1004@gmail.com>
2024-07-26 03:22:01 +0900

    feat: pdf 다운로드 내용 구성중

--- src/components/result/Answer.jsx
+++ src/components/result/Answer.jsx
@@ -55,38 +55,13 @@ const FeedbackDetailBox = styled.div`
     padding: 30px;
     white-space: pre-wrap;
 `;
-const PdfBtn = styled.div`
-    width: 250px;
-    font-size: 20px;
-    font-weight: 700;
-    border: 2px solid white;
-    padding: 5px;
-    text-align: center;
-    border-radius: 35px;
-    background-color: #fff;
-    cursor: pointer;
-    margin: 50px auto;
-`;
-function Answer({}) {
+
+function Answer({ }) {
     const [analyze, setAnalyze] = useRecoilState(myAnalyzeAtom);
-    const downloadPDF = () => {
-        const element = document.getElementById("pdf-download"); // PDF로 변환할 요소 선택
-        html2pdf(element, {
-          filename: "file.pdf", // default : file.pdf
-          html2canvas: { scale: 2 }, // 캡처한 이미지의 크기를 조절, 값이 클수록 더 선명하다.
-          jsPDF: { 
-            format: "b4",  // 종이 크기 형식
-            orientation: "portrait", // or landscape : 가로
-          },
-          callback: () => {
-            console.log("PDF 다운로드 완료");
-          },
-        });
-      };
+
     return (
         <>
-            <Swiper 
-                id="pdf-download" 
+            <Swiper
                 pagination={{
                     type: 'fraction',
                 }}
@@ -107,7 +82,7 @@ function Answer({}) {
 
             </Swiper>
             <AnswerFeedback />
-            <PdfBtn onClick={downloadPDF}>결과지 다운로드</PdfBtn>
+
         </>
     )
 }
--- src/components/result/DownloadPDF.jsx
+++ src/components/result/DownloadPDF.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { forwardRef } from 'react';
+import styled from 'styled-components';
+import Typo from '@components/Typography'
+import AnswerFeedback from './AnswerFeedback';
+import { useRecoilState } from 'recoil';
+import { myAnalyzeAtom } from '@store/atom';
+import AnswerContent from './AnswerContent';
+import html2pdf from 'html2pdf.js';
+
+const PdfBtn = styled.div`
+    width: 250px;
+    font-size: 20px;
+    font-weight: 700;
+    border: 2px solid white;
+    padding: 5px;
+    text-align: center;
+    border-radius: 35px;
+    background-color: #fff;
+    cursor: pointer;
+    margin: 50px auto;
+`;
 
 function DownloadPDF() {
-  return (
-    <div>DownloadPDF</div>
-  )
+    const [analyze, setAnalyze] = useRecoilState(myAnalyzeAtom);
+
+    const downloadPDF = () => {
+        const element = document.getElementById("pdf-download"); // PDF로 변환할 요소 선택
+        html2pdf(element, {
+            filename: "file.pdf", // default : file.pdf
+            html2canvas: { scale: 2 }, // 캡처한 이미지의 크기를 조절, 값이 클수록 더 선명하다.
+            jsPDF: {
+                format: "a4",  // 종이 크기 형식
+                orientation: "portrait", // or landscape : 가로
+            },
+            callback: () => {
+                console.log("PDF 다운로드 완료");
+            },
+        });
+    };
+    return (
+        <>
+            <div id="pdf-download" >DownloadPDF</div>
+            {/* {analyze.responses.map((item, i) => {
+                if (item.response) {
+                    return (
+                        <SwiperSlide key={i}>
+                            <AnswerContent answerIndex={i} />
+                        </SwiperSlide>
+                    )
+                }
+            })} */}
+            <PdfBtn onClick={downloadPDF}>결과지 다운로드</PdfBtn>
+        </>
+    )
 }
 
 export default DownloadPDF
