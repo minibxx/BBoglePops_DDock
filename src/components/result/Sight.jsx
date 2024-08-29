edaac3248f62b5e11ebd07eb74abd3e22827dd73
minibxx <alsdbqls1004@gmail.com>
2024-08-29 14:12:35 +0900

    feat: 시선 분석 하드코딩

--- src/components/result/Sight.jsx
+++ src/components/result/Sight.jsx
@@ -1,7 +1,22 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import { postEyeTrackingStop } from '../../apis/interview';
+import SightResult from '../../../dist/images/sightResult.png'
+import styled from 'styled-components';
+import Typo from '@components/Typography';
 
+const RectBorder2 = styled.div`
+    width: 270px;
+    margin-bottom: 30px;
+    padding-left: 5px;
+    color: white;
+`;
+const FeedbackBox = styled.div`
+    padding: 60px ;
+    border-radius: 40px;
+    border: 2px solid white;
+    color: white;
+`;
 function Sight() {
   const { interviewId } = useParams();
   const userId = localStorage.getItem('userId');
@@ -11,9 +26,20 @@ function Sight() {
   }, []);
 
   return (
-    <div className='text-[white]'>
-      가상 면접관의 얼굴 부분을 쳐다본 횟수가 가장 많습니다. 전체적으로 시선 처리가 매우 훌륭한 편
-    입니다. 다만, 배경으로 시선이 쏠리는 경우가 있어 그 부분은 앞으로 조금 더 유의하면 좋을 것 같습니다. </div>
+    <>
+      <img src={SightResult} className='w-[100%] p-[5%]' />
+
+      <FeedbackBox>
+        <RectBorder2>
+          <Typo title={'시선 분석 총평'} type={'body8'} />
+        </RectBorder2>
+          <div className='text-[white] text-[25px]'>
+            가상 면접관의 얼굴 부분을 쳐다본 횟수가 가장 많습니다. 전체적으로 시선 처리가 매우 훌륭한 편
+            입니다. 다만, 배경으로 시선이 쏠리는 경우가 있어 그 부분은 앞으로 조금 더 유의하면 좋을 것 같습니다. </div>
+
+
+      </FeedbackBox>
+    </>
   )
 }
 
